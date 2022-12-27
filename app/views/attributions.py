48c058b14f77a2232bd7fe9f7920ccfbf4b0f547
Jules257 <julesnzejimana@gmail.com>
2022-12-27 21:30:05 +0100

    Add attribution views

---
+++ app/views/attributions.py
@@ -0,0 +1,84 @@
+from django.http import HttpRequest
+from django.shortcuts import redirect,render
+from app.models import Attribution
+from app.forms import AttributionForm
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
+
+
+def index(request):
+    
+     # permission
+   
+    assert isinstance(request,HttpRequest)
+    
+    attributions = Attribution.objects.all()
+    return render(
+        request,
+        'app/attributions/index.html',
+        {
+            'attributions':attributions,
+            
+            
+        }
+    )
+    
+    
+        
+def create(request):
+    form =AttributionForm()
+    return render(
+        request,
+        'app/attributions/create.html',
+        {
+            'form': form
+            }
+        )
+    
+    
+def store(request):
+    if request.method == 'POST' :
+        form = AttributionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"attribution successfull saved")
+           
+        return redirect('/attributions')
+    
+    
+    
+
+def edit(request, id):
+    assert isinstance(request,HttpRequest)
+    if request.method == "GET" :
+        if id == 0:
+            form = AttributionForm()
+        else:
+            attribution = Attribution.objects.get(pk=id)
+            form = AttributionForm(instance=attribution)
+        return render(
+            request,
+            'app/attributions/edit.html',
+            {
+                'form': form
+            }
+        )
+        # update
+    else:
+        if id == 0:
+            form = AttributionForm(request.POST)
+        else:
+            attribution = Attribution.objects.get(pk=id)
+            form = AttributionForm(request.POST,instance=attribution)
+        if form.is_valid():
+            
+            form.save()
+            messages.success(request,"attribution successfull updated")
+        return redirect('/attributions') 
+    
+def delete(request, id):
+    attribution = Attribution.objects.get(pk=id)
+    attribution.delete()
+    return redirect('/attributions')         
