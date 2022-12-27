9688f176f199c159b3743f906cccd6d354605fff
EmaEmbete <embetemmelelwa@gmail.com>
2022-12-27 17:54:14 +0200

    Use classe_form as a module

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,4 +2,6 @@ from .enseignante_form import EnseignanteForm
 from .anneescolaire_form import AnneescolaireForm
 from .elev_form import ElevForm
 from .parent_form import ParentForm
+from .classe_form import ClasseForm
+
 
