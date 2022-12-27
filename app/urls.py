a18c46cc81dc117c43c7179ddbc07b96a7da7ab8
Senorita15 <nicolenimpaye@gmail.com>
2022-12-27 13:15:16 +0100

    Add enseignantes urls

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import home
+from app.views import home,enseignantes
 
 
 
 urlpatterns = [
     path('',home.index, name='home'),
-   
     
+    path('enseignantes/',enseignantes.index, name='enseignantes_index'),
+    path('enseignantes/create',enseignantes.create, name='enseignantes_create'),
+    path('enseignantes/store', enseignantes.store, name='enseignantes_store'),
+    path('enseignantes/edit/<int:id>', enseignantes.edit, name='enseignantes_edit'),
+    path('enseignantes/delete/<int:id>',enseignantes.delete, name='enseignantes_delete'),
     
     
     
