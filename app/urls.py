a8af1d26bebd0ebd1dae0ae7b7a00177e69ab4cb
EmaEmbete <embetemmelelwa@gmail.com>
2022-12-27 17:25:50 +0200

    Add elev url

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import home,enseignantes,anneescolaires
+from app.views import home,enseignantes,anneescolaires,elevs
 
 
 
@@ -19,6 +19,36 @@ urlpatterns = [
     path('anneescolaires/store', anneescolaires.store, name='anneescolaires_store'),
     path('anneescolaires/edit/<int:id>', anneescolaires.edit, name='anneescolaires_edit'),
     path('anneescolaires/delete/<int:id>', anneescolaires.delete, name='anneescolaires_delete'),
+
+
+    # path('cours/',cours.index, name='cours_index'),
+    # path('cours/create', cours.create, name='cours_create'),
+    # path('cours/store', cours.store, name='cours_store'),
+    # path('cours/edit/<int:id>', cours.edit, name='cours_edit'),
+    # path('cours/delete/<int:id>', cours.delete, name='cours_delete'),
+    
+    
+    path('elevs/',elevs.index, name='elevs_index'),
+    path('elevs/create', elevs.create, name='elevs_create'),
+    path('elevs/store', elevs.store, name='elevs_store'),
+    path('elevs/edit/<int:id>', elevs.edit, name='elevs_edit'),
+    path('elevs/delete/<int:id>', elevs.delete, name='elevs_delete'),
+
+    # path('parents/',parents.index, name='parents_index'),
+    # path('parents/create', parents.create, name='parents_create'),
+    # path('parents/store', parents.store, name='parents_store'),
+    # path('parents/edit/<int:id>', parents.edit, name='parents_edit'),
+    # path('parents/delete/<int:id>', parents.delete, name='parents_delete'),
+
+    #  path('inscriptions/',inscriptions.index, name='inscriptions_index'),
+    # path('inscriptions/create', inscriptions.create, name='inscriptions_create'),
+    # path('inscriptions/store', inscriptions.store, name='inscriptions_store'),
+    # path('inscriptions/edit/<int:id>', inscriptions.edit, name='inscriptions_edit'),
+    # path('inscriptions/delete/<int:id>', inscriptions.delete, name='inscriptions_delete')
+    
+    
+    
+    
     
     
     
