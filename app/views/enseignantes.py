36f9c19564c3f63e314e0694f208d97a9fffea9e
Senorita15 <nicolenimpaye@gmail.com>
2022-12-27 13:13:00 +0100

    Add enseignantes view

---
+++ app/views/enseignantes.py
@@ -0,0 +1,82 @@
+from django.http import HttpRequest
+from django.shortcuts import redirect,render
+from app.models import Enseignante
+from app.forms import EnseignanteForm
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
+
+
+def index(request):
+      # permission
+   
+    assert isinstance(request,HttpRequest)
+    
+    enseignantes = Enseignante.objects.all()
+    return render(
+        request,
+        'app/enseignantes/index.html',
+        {
+            'enseignantes':enseignantes,
+            
+            
+        }
+    )
+    
+    
+        
+def create(request):
+    form =EnseignanteForm()
+    return render(
+        request,
+        'app/enseignantes/create.html',
+        {
+            'form': form
+            }
+        )
+    
+    
+def store(request):
+    if request.method == 'POST' :
+        form = EnseignanteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"enseignant successfull saved")
+        return redirect('/enseignantes')
+    
+    
+    
+
+def edit(request, id):
+    assert isinstance(request,HttpRequest)
+    if request.method == "GET" :
+        if id == 0:
+            form = EnseignanteForm()
+        else:
+            enseignante = Enseignante.objects.get(pk=id)
+            form = EnseignanteForm(instance=elev)
+        return render(
+            request,
+            'app/enseignantes/edit.html',
+            {
+                'form': form
+            }
+        )
+        # update
+    else:
+        if id == 0:
+            form = EnseignanteForm(request.POST)
+        else:
+            enseignante = Enseignante.objects.get(pk=id)
+            form = EnseignanteForm(request.POST,instance=enseignante)
+        if form.is_valid():
+            
+            form.save()
+            messages.success(request,"enseignant successfull updated")
+        return redirect('/enseignantes') 
+    
+def delete(request, id):
+    elev = Enseignante.objects.get(pk=id)
+    enseignante.delete()
+    return redirect('/enseignantes')         
