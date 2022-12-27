232d0d3714d526f7444b2a6975767fd04585607b
Senorita15 <nicolenimpaye@gmail.com>
2022-12-27 10:04:22 +0100

    Add home url

---
+++ app/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from app.views import home
+
+
+
+urlpatterns = [
+    path('',home.index, name='home'),
+   
+    
+    
+    
+    
+    
+    
+    
+    
+   
+   
+  
+]
